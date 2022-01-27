931826fa0f0af2999829ad77d50cde072615721e
Luke42777 <55854062+Luke42777@users.noreply.github.com>
2022-01-27 19:27:26 +0000

    initial commit, html structure

---
+++ app.js
@@ -0,0 +1,19 @@
+class ListRemover extends React.Component{
+    state = {
+
+    }
+    
+
+    render(){
+        return(
+            <>
+                <div>Hello</div>
+            </>
+        )
+    }
+    
+    }
+    
+    
+    
+    ReactDOM.render(<React.StrictMode><ListRemover /></React.StrictMode>, document.getElementById("root"))
