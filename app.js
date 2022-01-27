267bd7b83eda488baf60280286bc7df1d2227a6c
Luke42777 <55854062+Luke42777@users.noreply.github.com>
2022-01-27 22:18:31 +0000

    working app

--- app.js
+++ app.js
@@ -1,19 +1,52 @@
-class ListRemover extends React.Component{
-    state = {
+const Person = (props) => {
+    return(
+        <li>
+        <span>{props.name}</span>
+        <button onClick={() => props.delete(props.id)}>Delete</button>
+        </li>
+    )
+}
 
+class List extends React.Component{
+    state = {
+        climbers : [
+            {
+                id: 1,
+                name: "Alex Honnold"
+            },
+            {
+                id: 2,
+                name: "Jerzy Kukuczka"
+            },
+            {
+                id: 3,
+                name: "Wojtek Kurtyka"
+            },
+            {
+                id: 4,
+                name: "Ueli Steck"
+            },
+    
+        ]
     }
     
+    handleDelete = id => {
+        let climbers = [...this.state.climbers];
+        climbers = climbers.filter(climber => climber.id !== id);
+        this.setState({
+            climbers,
+        })
+    }
 
     render(){
+        const climbers =   this.state.climbers.map( climber =>  <Person key={climber.id} id={climber.id} name={climber.name} delete={this.handleDelete} />);
         return(
             <>
-                <div>Hello</div>
+           {climbers}
             </>
         )
     }
     
     }
     
-    
-    
-    ReactDOM.render(<React.StrictMode><ListRemover /></React.StrictMode>, document.getElementById("root"))
+    ReactDOM.render(<React.StrictMode><List /></React.StrictMode>, document.getElementById("root"));
